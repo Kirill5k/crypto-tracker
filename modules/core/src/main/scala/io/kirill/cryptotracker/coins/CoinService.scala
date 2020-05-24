af597d910e66df91c97711fa5a6546390ef88b0e
kirill5k <immotional@aol.com>
2020-05-24 09:51:53 +0100

    coin service trait

--- modules/core/src/main/scala/io/kirill/cryptotracker/coins/CoinService.scala
+++ modules/core/src/main/scala/io/kirill/cryptotracker/coins/CoinService.scala
@@ -1,7 +1,21 @@
 package io.kirill.cryptotracker.coins
 
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import io.chrisdavenport.log4cats.Logger
+
 trait CoinService[F[_]] {
   def currentStats(coin: Cryptocoin): F[CoinStats]
 }
 
-object CoinService {}
+final private class LiveCoinService[F[_]: Sync: Logger](
+    val priceClient: CoinPriceClient[F],
+    val coinHistory: Ref[F, Map[CoinSymbol, CoinStats]]
+) extends CoinService[F] {
+
+  override def currentStats(coin: Cryptocoin): F[CoinStats] = ???
+}
+
+object CoinService {
+  def make[F[_]: Sync: Logger](priceClient: CoinPriceClient[F]): F[CoinService[F]] = ???
+}
