19a23dd6df8f38907934ec2cfe36199f47c50a7b
kirill5k <immotional@aol.com>
2020-05-24 10:36:59 +0100

    ref-based coin stats cache

--- modules/core/src/main/scala/io/kirill/cryptotracker/coins/CoinService.scala
+++ modules/core/src/main/scala/io/kirill/cryptotracker/coins/CoinService.scala
@@ -1,21 +1,28 @@
 package io.kirill.cryptotracker.coins
 
+import java.time.Instant
+
 import cats.effect.Sync
-import cats.effect.concurrent.Ref
-import io.chrisdavenport.log4cats.Logger
+import cats.implicits._
 
 trait CoinService[F[_]] {
   def currentStats(coin: Cryptocoin): F[CoinStats]
 }
 
-final private class LiveCoinService[F[_]: Sync: Logger](
+final private class LiveCoinService[F[_]: Sync](
     val priceClient: CoinPriceClient[F],
-    val coinHistory: Ref[F, Map[CoinSymbol, CoinStats]]
+    val coinStatsCache: CoinStatsCache[F]
 ) extends CoinService[F] {
 
-  override def currentStats(coin: Cryptocoin): F[CoinStats] = ???
+  override def currentStats(coin: Cryptocoin): F[CoinStats] =
+    for {
+      p <- priceClient.getCurrentPrice(coin)
+      s = CoinStats(coin, p, Instant.now)
+      _ <- coinStatsCache.add(s)
+    } yield s
 }
 
 object CoinService {
-  def make[F[_]: Sync: Logger](priceClient: CoinPriceClient[F]): F[CoinService[F]] = ???
+  def make[F[_]: Sync](priceClient: CoinPriceClient[F], coinStatsCache: CoinStatsCache[F]): F[CoinService[F]] =
+    Sync[F].delay(new LiveCoinService[F](priceClient, coinStatsCache))
 }
