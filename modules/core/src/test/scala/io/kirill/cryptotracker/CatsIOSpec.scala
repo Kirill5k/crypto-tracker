9fcf41946fd181eab7ae71e3b3f77ffb5227d323
kirill5k <immotional@aol.com>
2020-05-24 10:54:51 +0100

    ref-based coin stats cache tests

---
+++ modules/core/src/test/scala/io/kirill/cryptotracker/CatsIOSpec.scala
@@ -0,0 +1,18 @@
+package io.kirill.cryptotracker
+
+import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.effect.{ContextShift, IO}
+import io.chrisdavenport.log4cats.Logger
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import io.kirill.cryptotracker.config.AppConfig
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+import scala.concurrent.ExecutionContext
+import scala.io.Source
+
+trait CatsIOSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers {
+  implicit val ac: AppConfig        = AppConfig.load
+  implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.Implicits.global)
+  implicit val logger: Logger[IO]   = Slf4jLogger.getLogger[IO]
+}
---
+++ modules/core/src/test/scala/io/kirill/cryptotracker/coins/CoinStatsCacheSpec.scala
@@ -0,0 +1,59 @@
+package io.kirill.cryptotracker.coins
+
+import java.time.Instant
+
+import cats.effect.IO
+import cats.implicits._
+import io.kirill.cryptotracker.CatsIOSpec
+import squants.market.{GBP, Money}
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class CoinStatsCacheSpec extends CatsIOSpec {
+
+  "A RefCoinStatsCache" - {
+    "should store stats in cache" in {
+      val stats = List(
+        coinStats(timestamp = Instant.now().minusSeconds(1000)),
+        coinStats(timestamp = Instant.now().minusSeconds(100)),
+        coinStats(timestamp = Instant.now().minusSeconds(10))
+      )
+
+      val result = for {
+        cache <- CoinStatsCache.refCoinStatsCache[IO](2 hours, 5 minutes)
+        _     <- stats.map(s => cache.add(s)).sequence
+        res   <- cache.get(Bitcoin)
+      } yield res
+
+      result.asserting(_ must contain theSameElementsInOrderAs stats.reverse)
+    }
+
+    "should return empty list if no stats exist" in {
+      val result = for {
+        cache <- CoinStatsCache.refCoinStatsCache[IO](2 hours, 5 minutes)
+        res   <- cache.get(Bitcoin)
+      } yield res
+
+      result.asserting(_ must be (Nil))
+    }
+
+    "should clear expired stats" in {
+      val result = for {
+        cache <- CoinStatsCache.refCoinStatsCache[IO](2 seconds, 100 millis)
+        _ <- cache.add(coinStats(timestamp = Instant.now()))
+        < <- IO.sleep(3 seconds)
+        res   <- cache.get(Bitcoin)
+      } yield res
+
+      result.asserting(_ must be (Nil))
+    }
+  }
+
+  def coinStats(coin: Cryptocoin = Bitcoin, timestamp: Instant = Instant.now, price: Money = GBP(7500)) =
+    CoinStats(
+      coin,
+      CoinPrice(price, BigDecimal(0), BigDecimal(0), BigDecimal(0), BigDecimal(0)),
+      timestamp
+    )
+}
