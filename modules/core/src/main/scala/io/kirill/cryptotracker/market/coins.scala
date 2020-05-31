17e85b246b73f04a9935a6d69346042b2703bb6c
kirill5k <immotional@aol.com>
2020-05-31 21:05:27 +0100

    more coins

--- modules/core/src/main/scala/io/kirill/cryptotracker/market/coins.scala
+++ modules/core/src/main/scala/io/kirill/cryptotracker/market/coins.scala
@@ -20,4 +20,28 @@ object coins {
     val name   = CoinName("Ethereum")
   }
 
+  final case object Litecoin extends Cryptocoin {
+    override val symbol: CoinSymbol = CoinSymbol("LTC")
+    override val name: CoinName = CoinName("Litecoin")
+  }
+
+  final case object Ethereumclassic extends Cryptocoin {
+    override val symbol: CoinSymbol = CoinSymbol("ETC")
+    override val name: CoinName = CoinName("Ethereumclassic")
+  }
+
+  final case object Bitcoincash extends Cryptocoin {
+    override val symbol: CoinSymbol = CoinSymbol("BCH")
+    override val name: CoinName = CoinName("Bitcoincash")
+  }
+
+  final case object Ripple extends Cryptocoin {
+    override val symbol: CoinSymbol = CoinSymbol("XRP")
+    override val name: CoinName = CoinName("Ripple")
+  }
+
+  final case object Neo extends Cryptocoin {
+    override val symbol: CoinSymbol = CoinSymbol("NEO")
+    override val name: CoinName = CoinName("Neo")
+  }
 }
