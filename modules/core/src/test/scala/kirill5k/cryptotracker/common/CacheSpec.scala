21fba9cb53584c391397d456a8a902ecb9649992
kirill5k <immotional@aol.com>
2021-02-11 18:20:50 +0000

    cache tests

---
+++ modules/core/src/test/scala/kirill5k/cryptotracker/common/CacheSpec.scala
@@ -0,0 +1,54 @@
+package kirill5k.cryptotracker.common
+
+import cats.effect.IO
+import kirill5k.cryptotracker.CatsIOSpec
+
+import scala.concurrent.duration._
+
+class CacheSpec extends CatsIOSpec {
+  "A RefbasedCache" should {
+
+    "return non-expired elements" in {
+      val result = for {
+        cache <- Cache.make[IO, String, String](5.seconds, 1.second)
+        _     <- cache.put("foo", "bar")
+        _     <- IO.sleep(2.seconds)
+        res   <- cache.get("foo")
+      } yield res
+
+      result.unsafeToFuture().map(_ mustBe Some("bar"))
+    }
+
+    "return empty option when element expires" in {
+      val result = for {
+        cache <- Cache.make[IO, String, String](1.seconds, 1.second)
+        _     <- cache.put("foo", "bar")
+        _     <- IO.sleep(2.seconds)
+        res   <- cache.get("foo")
+      } yield res
+
+      result.unsafeToFuture().map(_ mustBe None)
+    }
+
+    "return true when item is present in cache" in {
+      val result = for {
+        cache <- Cache.make[IO, String, String](1.seconds, 1.second)
+        _     <- cache.put("foo", "bar")
+        res   <- cache.contains("foo")
+      } yield res
+
+      result.unsafeToFuture().map(_ mustBe true)
+    }
+
+    "return false when item has expired" in {
+      val result = for {
+        cache <- Cache.make[IO, String, String](1.seconds, 1.second)
+        _     <- cache.put("foo", "bar")
+        _     <- IO.sleep(2.seconds)
+        res   <- cache.contains("foo")
+      } yield res
+
+      result.unsafeToFuture().map(_ mustBe false)
+    }
+  }
+}
