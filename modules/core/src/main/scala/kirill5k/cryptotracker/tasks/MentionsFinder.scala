3fed6cb58c7ff8b47d2dfd19c7f082f4d5a7b0c4
kirill5k <immotional@aol.com>
2021-02-13 10:53:45 +0000

    error handling in tasks

--- modules/core/src/main/scala/kirill5k/cryptotracker/tasks/MentionsFinder.scala
+++ modules/core/src/main/scala/kirill5k/cryptotracker/tasks/MentionsFinder.scala
@@ -1,16 +1,14 @@
 package kirill5k.cryptotracker.tasks
 
 import cats.effect.{Concurrent, Sync}
+import fs2.Stream
 import kirill5k.cryptotracker.common.config.RedditConfig
 import kirill5k.cryptotracker.services.MentionService
-import fs2.Stream
-
-trait MentionsFinder[F[_]] extends Task[F] {}
 
 final private class RedditMentionsFinder[F[_]: Concurrent](
     private val redditConfig: RedditConfig,
     private val mentionService: MentionService[F]
-) extends MentionsFinder[F] {
+) extends Task[F] {
 
   override def run(): fs2.Stream[F, Unit] =
     Stream
@@ -18,10 +16,9 @@ final private class RedditMentionsFinder[F[_]: Concurrent](
       .map(sub => mentionService.liveFromReddit(sub, redditConfig.searchPeriod))
       .parJoinUnbounded
       .drain
-
 }
 
-object MentionsFinder {
-  def reddit[F[_]: Concurrent](redditConfig: RedditConfig, mentionService: MentionService[F]): F[Task[F]] =
+object RedditMentionsFinder {
+  def make[F[_]: Concurrent](redditConfig: RedditConfig, mentionService: MentionService[F]): F[Task[F]] =
     Sync[F].delay(new RedditMentionsFinder[F](redditConfig, mentionService))
 }
--- modules/core/src/main/scala/kirill5k/cryptotracker/tasks/Tasks.scala
+++ modules/core/src/main/scala/kirill5k/cryptotracker/tasks/Tasks.scala
@@ -1,25 +1,36 @@
 package kirill5k.cryptotracker.tasks
 
 import cats.Parallel
-import cats.effect.Concurrent
+import cats.effect.{Concurrent, Timer}
 import cats.implicits._
 import fs2.Stream
+import io.chrisdavenport.log4cats.Logger
 import kirill5k.cryptotracker.common.config.AppConfig
 import kirill5k.cryptotracker.services.Services
 
-final case class Tasks[F[_]: Concurrent](
+import scala.concurrent.duration._
+
+final case class Tasks[F[_]: Concurrent: Logger: Timer](
     tasks: List[Task[F]]
 ) {
 
   def runAll(): Stream[F, Unit] =
     Stream
-      .emits(tasks.map(_.run()))
+      .emits(tasks.map(_.run().resumeOnError(1.minute)))
       .parJoinUnbounded
+
+  implicit final private class StreamOps[O](private val stream: Stream[F, O]) {
+    def resumeOnError(delay: FiniteDuration)(implicit logger: Logger[F], timer: Timer[F]): Stream[F, O] =
+      stream.handleErrorWith { error =>
+        Stream.eval_(logger.error(error)("error during task processing")) ++
+          stream.delayBy(delay)(timer)
+      }
+  }
 }
 
 object Tasks {
-  def make[F[_]: Parallel: Concurrent](config: AppConfig, services: Services[F]): F[Tasks[F]] =
+  def make[F[_]: Parallel: Concurrent: Logger: Timer](config: AppConfig, services: Services[F]): F[Tasks[F]] =
     List(
-      MentionsFinder.reddit[F](config.reddit, services.mention)
+      RedditMentionsFinder.make[F](config.reddit, services.mention)
     ).parSequence.map(Tasks[F])
 }
