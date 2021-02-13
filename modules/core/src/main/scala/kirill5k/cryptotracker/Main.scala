6e23445e129c238b76a0ca15281c14949f3ae365
kirill5k <immotional@aol.com>
2021-02-13 09:57:17 +0000

    services and clinets modules

--- modules/core/src/main/scala/kirill5k/cryptotracker/Main.scala
+++ modules/core/src/main/scala/kirill5k/cryptotracker/Main.scala
@@ -1,8 +1,12 @@
 package kirill5k.cryptotracker
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{Blocker, ExitCode, IO, IOApp}
 import io.chrisdavenport.log4cats.Logger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import kirill5k.cryptotracker.clients.Clients
+import kirill5k.cryptotracker.common.Resources
+import kirill5k.cryptotracker.common.config.AppConfig
+import kirill5k.cryptotracker.services.Services
 
 object Main extends IOApp {
 
@@ -10,6 +14,13 @@ object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] =
     for {
-      _ <- logger.info(s"Hello, World!")
+      _      <- logger.info("starting crypto tracker")
+      config <- Blocker[IO].use(AppConfig.load[IO]) <* logger.info("loaded config")
+      _ <- Resources.make[IO].use { resources =>
+        for {
+          clients  <- Clients.make[IO](config, resources.sttpBackend)
+          services <- Services.make[IO](clients)
+        } yield ()
+      }
     } yield ExitCode.Success
 }
