83c050b977e96cc6f4fa323f29b14b60d8063138
kirill5k <immotional@aol.com>
2021-02-19 14:49:54 +0000

    use newer api

--- modules/core/src/main/scala/kirill5k/cryptotracker/clients/reddit/RedditClient.scala
+++ modules/core/src/main/scala/kirill5k/cryptotracker/clients/reddit/RedditClient.scala
@@ -3,13 +3,13 @@ package kirill5k.cryptotracker.clients.reddit
 import cats.effect.{Sync, Timer}
 import cats.implicits._
 import io.chrisdavenport.log4cats.Logger
-import kirill5k.cryptotracker.common.config.RedditConfig
-import kirill5k.cryptotracker.domain.{Mention, Subreddit}
 import io.circe.generic.auto._
 import kirill5k.cryptotracker.clients.reddit.mappers.MentionsMapper
 import kirill5k.cryptotracker.clients.reddit.responses.RedditSubmissionsResponse
+import kirill5k.cryptotracker.common.config.RedditConfig
 import kirill5k.cryptotracker.common.json._
 import kirill5k.cryptotracker.common.errors.AppError
+import kirill5k.cryptotracker.domain.{Mention, Subreddit}
 import sttp.client3._
 import sttp.client3.circe.asJson
 
@@ -28,23 +28,25 @@ final private class LiveRedditClient[F[_]: Sync](
 ) extends RedditClient[F] {
 
   override def findMentions(subreddit: Subreddit, duration: FiniteDuration): F[List[Mention]] = {
-    val dur = duration.toString().replaceFirst(" ", "").replaceFirst("(?<=[a-z])\\w+", "")
-    basicRequest
-      .get(uri"${config.baseUri}/reddit/search/submission?subreddit=${subreddit.value}&over_18=true&after=$dur")
-      .response(asJson[RedditSubmissionsResponse])
-      .send(backend)
-      .flatMap { r =>
-        r.body match {
-          case Right(response) =>
-            response.data.flatMap(MentionsMapper.map).pure[F]
-          case Left(DeserializationException(body, error)) =>
-            logger.error(s"error parsing json: ${error.getMessage}\n$body") *>
-              AppError.Json(s"error parsing json: ${error.getMessage}").raiseError[F, List[Mention]]
-          case Left(error) =>
-            logger.error(s"error get submissions from reddit: ${r.code}\n$error") *>
-              timer.sleep(5.second) *> findMentions(subreddit, duration + 5.second)
+    timer.clock.realTime(SECONDS).flatMap { epocSeconds =>
+      val dur = epocSeconds - duration.toSeconds
+      basicRequest
+        .get(uri"${config.baseUri}/reddit/submission/search?subreddit=${subreddit.value}&over_18=true&after=$dur")
+        .response(asJson[RedditSubmissionsResponse])
+        .send(backend)
+        .flatMap { r =>
+          r.body match {
+            case Right(response) =>
+              response.data.flatMap(MentionsMapper.map).pure[F]
+            case Left(DeserializationException(body, error)) =>
+              logger.error(s"error parsing json: ${error.getMessage}\n$body") *>
+                AppError.Json(s"error parsing json: ${error.getMessage}").raiseError[F, List[Mention]]
+            case Left(error) =>
+              logger.error(s"error get submissions from reddit: ${r.code}\n$error") *>
+                timer.sleep(5.second) *> findMentions(subreddit, duration + 5.second)
+          }
         }
-      }
+    }
   }
 }
 
--- modules/core/src/test/scala/kirill5k/cryptotracker/clients/reddit/RedditClientSpec.scala
+++ modules/core/src/test/scala/kirill5k/cryptotracker/clients/reddit/RedditClientSpec.scala
@@ -1,29 +1,33 @@
 package kirill5k.cryptotracker.clients.reddit
 
-import cats.effect.IO
+import cats.effect.{Clock, IO, Timer}
 import kirill5k.cryptotracker.RequestOps._
 import kirill5k.cryptotracker.SttpClientSpec
 import kirill5k.cryptotracker.common.config.RedditConfig
 import kirill5k.cryptotracker.domain.{Subreddit, Ticker}
 import sttp.client3.{Response, SttpBackend}
 
+import java.time.Instant
 import scala.concurrent.duration._
 
 class RedditClientSpec extends SttpClientSpec {
 
   val subreddit = Subreddit("WallStreetBets")
   val config    = RedditConfig("http://reddit.com", 5.minutes, List(subreddit))
+  val timestamp = Instant.parse("2020-01-01T00:25:00Z")
 
   "A RedditClient" should {
 
     "return list of stock/crypto mentions from a given subreddit" in {
-      val submissionsEndpoint = "reddit/search/submission"
-      val params              = Map("after" -> "5m", "subreddit" -> "WallStreetBets", "over_18" -> "true")
+//      implicit val timer = mockTimer(timestamp.getEpochSecond)
+
+      val submissionsEndpoint = "reddit/submission/search"
+      val params              = Map("subreddit" -> "WallStreetBets", "over_18" -> "true", "after" -> "1577838000")
       val testingBackend: SttpBackend[IO, Any] = backendStub
         .whenRequestMatchesPartial {
           case r if r.isGet && r.isGoingTo(s"reddit.com/$submissionsEndpoint") && r.hasParams(params) =>
             Response.ok(json("reddit/submissions-response.json"))
-          case _ => throw new RuntimeException()
+          case r => throw new RuntimeException(r.uri.toString())
         }
 
       val telegramClient = RedditClient.make[IO](config, testingBackend)
@@ -44,5 +48,13 @@ class RedditClientSpec extends SttpClientSpec {
         )
       }
     }
+  }
+
+  def mockTimer(timeSeconds: Long): Timer[IO] = new Timer[IO] {
+    override def clock: Clock[IO] = new Clock[IO] {
+      override def realTime(unit: TimeUnit): IO[Long] = IO.pure(timeSeconds)
+      override def monotonic(unit: TimeUnit): IO[Long] = IO.pure(timeSeconds)
+    }
+    override def sleep(duration: FiniteDuration): IO[Unit] = IO.unit
   }
 }
