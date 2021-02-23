26885ff90c61f757a86eef84a1a1c002a1ad26c0
kirill5k <immotional@aol.com>
2021-02-23 09:04:21 +0000

    summaries endpoint

--- modules/core/src/main/scala/kirill5k/cryptotracker/controllers/MentionController.scala
+++ modules/core/src/main/scala/kirill5k/cryptotracker/controllers/MentionController.scala
@@ -5,8 +5,12 @@ import cats.implicits._
 import io.circe.generic.auto._
 import io.chrisdavenport.log4cats.Logger
 import kirill5k.cryptotracker.common.errors.AppError.MissingQueryParam
+import kirill5k.cryptotracker.controllers.MentionController.{DateRange, MentionSummaries}
+import kirill5k.cryptotracker.domain.{Mention, Ticker}
 import kirill5k.cryptotracker.services.MentionService
 import org.http4s.HttpRoutes
+
+import java.time.Instant
 
 final private class MentionController[F[_]](
     private val service: MentionService[F]
@@ -17,10 +21,17 @@ final private class MentionController[F[_]](
       case GET -> Root / "mentions" :? OptionalDateFromQueryParam(from) +& OptionalDateToQueryParam(to) =>
         withErrorHandling {
           for {
-            dateFrom <- F.fromOption(from, MissingQueryParam("from"))
-            dateTo   <- F.fromOption(to, MissingQueryParam("to"))
-            mentions <- service.findAll(dateFrom, dateTo)
-            res      <- Ok(mentions)
+            dateRange <- fromAndTo(from, to)
+            mentions  <- service.findAll(dateRange.from, dateRange.to)
+            res       <- Ok(mentions)
+          } yield res
+        }
+      case GET -> Root / "mentions" / "summary" :? OptionalDateFromQueryParam(from) +& OptionalDateToQueryParam(to) =>
+        withErrorHandling {
+          for {
+            dateRange <- fromAndTo(from, to)
+            mentions  <- service.findAll(dateRange.from, dateRange.to)
+            res       <- Ok(MentionSummaries.from(mentions, dateRange))
           } yield res
         }
       case GET -> Root / "mentions" / TickerVar(ticker) :? OptionalDateFromQueryParam(from) +& OptionalDateToQueryParam(to) =>
@@ -28,9 +39,38 @@ final private class MentionController[F[_]](
           service.findBy(ticker, from, to).flatMap(Ok(_))
         }
     }
+
+  private def fromAndTo(from: Option[Instant], to: Option[Instant])(implicit F: Sync[F]): F[DateRange] =
+    (F.fromOption(from, MissingQueryParam("from")), F.fromOption(to, MissingQueryParam("to"))).mapN(DateRange.apply)
 }
 
 object MentionController {
+
+  final case class DateRange(
+      from: Instant,
+      to: Instant
+  )
+
+  final case class MentionSummary(
+      ticker: Ticker,
+      total: Int,
+      times: List[Instant]
+  )
+
+  final case class MentionSummaries(
+      dateRange: DateRange,
+      summaries: List[MentionSummary]
+  )
+
+  object MentionSummaries {
+    def from(mentions: List[Mention], dateRange: DateRange): MentionSummaries = {
+      val summaries = mentions
+        .groupBy(_.ticker)
+        .map { case (t, ms) => MentionSummary(t, ms.size, ms.map(_.time)) }
+        .toList
+      MentionSummaries(dateRange, summaries)
+    }
+  }
 
   def make[F[_]: Sync](service: MentionService[F]): F[Controller[F]] =
     Sync[F].delay(new MentionController[F](service))
