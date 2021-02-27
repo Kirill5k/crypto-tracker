4449b18f178c047b5a4ac1f87101eb2e80284951
kirill5k <immotional@aol.com>
2021-02-27 10:03:35 +0000

    add navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
+    <div>
+      <b-navbar type="dark" variant="dark">
+        <b-navbar-nav>
+          <b-nav-item to="/>Home</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import { BNavbar, BNavbarNav, BNavItem } from 'bootstrap-vue'
+
+export default {
+  name: 'App',
+  components: { BNavbar, BNavbarNav, BNavItem }
+}
+</script>
+
 
 <style lang="scss">
 @import 'assets/bootstrap-overrides.scss';
 @import '~bootstrap/scss/bootstrap.scss';
 @import '~bootstrap-vue/src/index.scss';
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
