23a0033460136c5716165a6d9c20833dca4c757b
kirill5k <immotional@aol.com>
2021-03-03 09:51:04 +0000

    pass ticker mention props to timeseries chart

---
+++ frontend/src/components/TickerMentionsTimeseriesChart.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="ticker-mentions-timeseries-chart">
+    <line-chart
+      :ticker="ticker"
+      :chart-data="chartData"
+      :options="options"
+    />
+  </div>
+</template>
+
+<script>
+import LineChart from './charts/LineChart.js'
+
+const DEFAULT_OPTIONS = {
+  responsive: true,
+  legend: {
+    display: true
+  },
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      type: 'time',
+      time: {
+        unit: 'hour',
+        displayFormats: {
+          quarter: 'HH, MM'
+        }
+      }
+    }
+  }
+}
+
+export default {
+  name: 'TickerMentionsTimeseriesChart',
+  components: { LineChart },
+  props: {
+    ticker: {
+      type: String,
+      required: true
+    },
+    mentionTimes: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    options () {
+      return {
+        ...DEFAULT_OPTIONS
+      }
+    },
+    chartData () {
+      const labels = []
+      const datasets = [{
+        label: this.ticker,
+        backgroundColor: '#03c2fc',
+        data: this.mentionTimes.map(t => new Date(t).getTime())
+      }]
+      return { labels, datasets }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,18 +6,29 @@
     <mentions-popularity-chart
       v-if="mentions.length"
       :mentions="mentions"
+      @click="displayTickerMentionTimeseries"
+    />
+    <ticker-mentions-timeseries-chart
+      v-if="tickerMentionTimes.length"
+      :ticker="selectedTicker"
+      :mention-times="tickerMentionTimes"
     />
   </b-container>
 </template>
 
 <script>
 import { BContainer } from 'bootstrap-vue'
-import DateRangePicker from '@/components/DateRangePicker.vue'
-import MentionsPopularityChart from '@/components/MentionsPopularityChart.vue'
+import DateRangePicker from '@/components/DateRangePicker'
+import MentionsPopularityChart from '@/components/MentionsPopularityChart'
+import TickerMentionsTimeseriesChart from '@/components/TickerMentionsTimeseriesChart'
 
 export default {
   name: 'Home',
-  components: { BContainer, DateRangePicker, MentionsPopularityChart },
+  components: { BContainer, DateRangePicker, MentionsPopularityChart, TickerMentionsTimeseriesChart },
+  data: () => ({
+    selectedTicker: null,
+    tickerMentionTimes: []
+  }),
   computed: {
     mentions () {
       return this.$store.state.mentions
@@ -26,6 +37,9 @@ export default {
   methods: {
     getMentions (dates) {
       this.$store.dispatch('getMentions', dates)
+    },
+    displayTickerMentionTimeseries (ticker) {
+      console.log(ticker)
     }
   }
 }
