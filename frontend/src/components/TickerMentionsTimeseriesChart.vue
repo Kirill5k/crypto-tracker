93cb3942c9e9e578e2c5a5aa4ff32a2645ad1f8e
kirill5k <immotional@aol.com>
2021-03-05 13:31:39 +0000

    update x-axis labels

--- frontend/src/components/TickerMentionsTimeseriesChart.vue
+++ frontend/src/components/TickerMentionsTimeseriesChart.vue
@@ -48,7 +48,7 @@ export default {
     },
     chartData () {
       const mentionsByHour = this.mentionsCountedByHour
-      const labels = this.dates.map(d => `${d.getHours()}, ${d.getDate()}`)
+      const labels = this.dates.map(d => `${d.getHours()}:00, ${d.getDate()}/${d.getMonth()}`)
       console.log(this.dateFrom, this.dateTo, this.dates.length)
       const datasets = [{
         label: `${this.ticker} mentions`,
