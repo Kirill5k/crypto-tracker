d5b6b8b9d784db015c381aee3a78b8620bb841d5
kirill5k <immotional@aol.com>
2021-03-06 15:20:39 +0000

    adjust styles

--- frontend/src/components/MentionsSidebar.vue
+++ frontend/src/components/MentionsSidebar.vue
@@ -8,18 +8,12 @@
       shadow
     >
       <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <ul>
-          <li v-for="mention in mentions" :key="mention.time">
-            <p class="mb-0">{{ mention.message }}</p>
-            <a :href="mention.url" target="_blank" class="mb-1">
-              /r/{{ mention.source.subreddit }}
-            </a>
-          </li>
-        </ul>
+        <div v-for="mention in mentions" :key="mention.time" class="mb-1">
+          <p class="mb-0">{{ mention.message }}</p>
+          <a :href="mention.url" target="_blank" class="badge badge-primary text-wrap">
+            /r/{{ mention.source.subreddit }}
+          </a>
+        </div>
       </div>
     </b-sidebar>
   </div>
