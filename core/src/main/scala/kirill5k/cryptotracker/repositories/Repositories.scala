6dd94b297128ef427f8637371b8c1f53dfeeca3f
kirill5k <immotional@aol.com>
2021-03-15 09:25:38 +0000

    update repositories module

--- core/src/main/scala/kirill5k/cryptotracker/repositories/Repositories.scala
+++ core/src/main/scala/kirill5k/cryptotracker/repositories/Repositories.scala
@@ -5,12 +5,12 @@ import cats.implicits._
 import mongo4cats.client.MongoClientF
 
 final case class Repositories[F[_]](
-    mention: MentionRepository[F]
+    mention: MentionRepository[F],
+    companyRepository: CompanyRepository[F]
 )
 
 object Repositories {
   def make[F[_]: Concurrent](client: MongoClientF[F]): F[Repositories[F]] =
-    MentionRepository
-      .make[F](client)
-      .map(Repositories[F])
+    (MentionRepository.make[F](client), CompanyRepository.make[F](client))
+      .mapN(Repositories[F])
 }
