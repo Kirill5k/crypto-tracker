34f986f9955ee6973329c8007cc1b1ea42313f12
kirill5k <immotional@aol.com>
2021-03-17 10:39:13 +0000

    CompanyService

--- core/src/main/scala/kirill5k/cryptotracker/common/errors.scala
+++ core/src/main/scala/kirill5k/cryptotracker/common/errors.scala
@@ -1,5 +1,7 @@
 package kirill5k.cryptotracker.common
 
+import kirill5k.cryptotracker.domain.Ticker
+
 object errors {
   sealed trait AppError extends Throwable {
     val message: String
@@ -7,6 +9,10 @@ object errors {
   }
 
   object AppError {
+    final case class CompanyNotFound(ticker: Ticker) extends AppError {
+      override val message: String = s"company ${ticker.value} cannot be found"
+    }
+
     final case class MissingQueryParam(name: String)     extends AppError {
       override val message: String = s"query parameter '$name' is required"
     }
--- core/src/main/scala/kirill5k/cryptotracker/services/CompanyService.scala
+++ core/src/main/scala/kirill5k/cryptotracker/services/CompanyService.scala
@@ -1,8 +1,9 @@
 package kirill5k.cryptotracker.services
 
-import cats.Monad
+import cats.MonadError
 import cats.implicits._
 import kirill5k.cryptotracker.clients.alphavantage.AlphaVantageClient
+import kirill5k.cryptotracker.common.errors.AppError.CompanyNotFound
 import kirill5k.cryptotracker.domain.{Company, Ticker}
 import kirill5k.cryptotracker.repositories.CompanyRepository
 
@@ -10,13 +11,20 @@ trait CompanyService[F[_]] {
   def find(ticker: Ticker): F[Company]
 }
 
-private final class LiveCompanyService[F[_]: Monad](
+private final class LiveCompanyService[F[_]](
     private val alphaVantageClient: AlphaVantageClient[F],
     private val companyRepository: CompanyRepository[F]
-) extends CompanyService[F] {
+)(implicit F: MonadError[F, Throwable]) extends CompanyService[F] {
   override def find(ticker: Ticker): F[Company] =
-    companyRepository.existsBy(ticker).flatMap {
-      case true => companyRepository.findBy(ticker)
-      case false => alphaVantageClient.findCompany(ticker).map(_.get)
+    companyRepository.findBy(ticker).flatMap {
+      case Some(company) => company.pure[F]
+      case None => searchForCompany(ticker)
     }
+
+  private def searchForCompany(ticker: Ticker): F[Company] =
+    alphaVantageClient.findCompany(ticker).flatMap {
+      case Some(company) => companyRepository.save(company) *> company.pure[F]
+      case None => CompanyNotFound(ticker).raiseError[F, Company]
+    }
+
 }
